52acbcf14dd89f9df8124abb4211eb21a6649ab9
L1-An <shiyu46@icloud.com>
2024-05-26 15:45:23 +0200

    [*] 新功能(1.5.5): 新增chance方法，可以方便处理概率

--- src/main/kotlin/org/serverct/parrot/parrotx/function/Math.kt
+++ src/main/kotlin/org/serverct/parrot/parrotx/function/Math.kt
@@ -21,3 +21,10 @@ fun Int.last(range: IntRange): Int {
 }
 
 fun Double.round(scale: Int = 2): Double = toBigDecimal().setScale(scale, RoundingMode.HALF_DOWN).toDouble()
+
+/**
+ * 以 value 的概率返回 true
+ * @param value 概率值
+ * @param max 最大值(也就是 100% 的值, 默认为 100)
+ */
+fun chance(value: Double, max: Int = 100) : Boolean = Math.random() * max < value
